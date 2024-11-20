9566a4efcccdb6ef898e52bbc52901e2a3193e64
Juras Ezerskis <ezerskis.juras@gmail.com>
2024-11-20 20:03:06 +0200

    Removing all old Models

--- api/Enums/Roles.cs
+++
@@ -1,9 +0,0 @@
-namespace api.Enums
-{
-    public enum Roles
-    {
-        SuperAdmin,
-        BusinessOwner,
-        Employee
-    }
-}
--- api/Enums/TaxType.cs
+++
@@ -1,9 +0,0 @@
-namespace api.Enums
-{
-    public enum TaxType
-    {
-        VAT,
-        FoodTax,
-        AlcoholTax
-    }
-}
