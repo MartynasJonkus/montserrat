551a6bc1728e2ecc0a0c6596639df5bbe115e594
Jokūbas Šilgalys <jokubas.silgalys@gmail.com>
2024-11-25 19:21:44 +0200

    Added order creation page
    Made an order creation page similar to the one seen in page 12 of the design document

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,194 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { VscAccount } from "react-icons/vsc";
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+//import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div id="top-nav">
+                <div id="top-nav-left">
+                    <div className="circle" />
+                    <div className="page-button">Products</div>
+                    <div className="page-button">Reservations</div>
+                    <div className="page-button">Taxes</div>
+                </div>
+                <div id="top-nav-right">
+                    <div className="page-button">Employees</div>
+                </div>
+            </div>
+
+            <div id="order-container">
+                <div id="container-left">
+                    <div id="container-top" />
+                    <div id="order-details">
+                        <div id="order-top"><b>CURRENT ORDER</b></div>
+                        <hr />
+                        <div className="order-product">
+                            <div id="order-product-amount">x2</div>
+                            <div id="order-product-name">Chocolate chip cookie</div>
+                            <div id="order-product-price">$2.00</div>
+                        </div>
+                        <hr />
+                        <div className="order-product">
+                            <div id="order-product-amount">x2</div>
+                            <div id="order-product-name">Chocolate chip cookie</div>
+                            <div id="order-product-price">$2.00</div>
+                        </div>
+                        <hr />
+                        <div className="order-product">
+                            <div id="order-product-amount">x2</div>
+                            <div id="order-product-name">Chocolate chip cookie</div>
+                            <div id="order-product-price">$2.00</div>
+                        </div>
+                        <hr />
+                        <div className="order-product">
+                            <div id="order-product-amount">x2</div>
+                            <div id="order-product-name">Chocolate chip cookie</div>
+                            <div id="order-product-price">$2.00</div>
+                        </div>
+                        <hr />
+                        <div className="order-product">
+                            <div id="order-product-amount">x2</div>
+                            <div id="order-product-name">Chocolate chip cookie</div>
+                            <div id="order-product-price">$2.00</div>
+                        </div>
+                        <hr />
+                        <div className="order-product">
+                            <div id="order-product-amount">x2</div>
+                            <div id="order-product-name">Chocolate chip cookie</div>
+                            <div id="order-product-price">$2.00</div>
+                        </div>
+                        <hr />
+                        <div className="order-product">
+                            <div id="order-product-amount">x2</div>
+                            <div id="order-product-name">Chocolate chip cookie</div>
+                            <div id="order-product-price">$2.00</div>
+                        </div>
+                    </div>
+                    <div id="order-price">
+                        <div id="prices">
+                            <div className="price-component">
+                                <div className="component-name">Subtotal</div>
+                                <div className="price-amount">$16.00</div>
+                            </div>
+                            <div className="price-component">
+                                <div className="component-name">Discount</div>
+                                <div className="price-amount">$0</div>
+                            </div>
+                        </div>
+                        <hr />
+                        <div id="price-bottom">
+                            <div className="component-name">Total</div>
+                            <div className="price-amount">$16.00</div>
+                        </div>
+                    </div>
+                    <div id="container-bottom">
+                        <div id="payment-split">Split</div>
+                        <div id="payment-pay">Pay</div>
+                    </div>
+                </div>
+
+
+                <div id="container-right">
+                    <div id="container-top">Products</div>
+                    <div id="products">
+                        <div className="product-group">
+                            <div className="group-name">Coffee</div>
+                            <div className="items">
+                                <div className="item">
+                                    <div className="item-name">Latte</div>
+                                    <div className="item-price">$2.50</div>
+                                </div>
+                                <div className="item">
+                                    <div className="item-name">Latte</div>
+                                    <div className="item-price">$2.50</div>
+                                </div>
+                                <div className="item">
+                                    <div className="item-name">Latte</div>
+                                    <div className="item-price">$2.50</div>
+                                </div>
+                                <div className="item">
+                                    <div className="item-name">Latte</div>
+                                    <div className="item-price">$2.50</div>
+                                </div>
+                                <div className="item">
+                                    <div className="item-name">Latte</div>
+                                    <div className="item-price">$2.50</div>
+                                </div>
+                                <div className="item">
+                                    <div className="item-name">Latte</div>
+                                    <div className="item-price">$2.50</div>
+                                </div>
+                                <div className="item">
+                                    <div className="item-name">Latte</div>
+                                    <div className="item-price">$2.50</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="product-group">
+                            <div className="group-name">Cookies</div>
+                            <div className="items">
+                                <div className="item">
+                                    <div className="item-name">Latte</div>
+                                    <div className="item-price">$2.50</div>
+                                </div>
+                                <div className="item">
+                                    <div className="item-name">Latte</div>
+                                    <div className="item-price">$2.50</div>
+                                </div>
+                                <div className="item">
+                                    <div className="item-name">Latte</div>
+                                    <div className="item-price">$2.50</div>
+                                </div>
+                                <div className="item">
+                                    <div className="item-name">Latte</div>
+                                    <div className="item-price">$2.50</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="product-group">
+                            <div className="group-name">Coffee</div>
+                            <div className="items">
+                                <div className="item">
+                                    <div className="item-name">Latte</div>
+                                    <div className="item-price">$2.50</div>
+                                </div>
+                                <div className="item">
+                                    <div className="item-name">Latte</div>
+                                    <div className="item-price">$2.50</div>
+                                </div>
+                                <div className="item">
+                                    <div className="item-name">Latte</div>
+                                    <div className="item-price">$2.50</div>
+                                </div>
+                                <div className="item">
+                                    <div className="item-name">Latte</div>
+                                    <div className="item-price">$2.50</div>
+                                </div>
+                                <div className="item">
+                                    <div className="item-name">Latte</div>
+                                    <div className="item-price">$2.50</div>
+                                </div>
+                                <div className="item">
+                                    <div className="item-name">Latte</div>
+                                    <div className="item-price">$2.50</div>
+                                </div>
+                                <div className="item">
+                                    <div className="item-name">Latte</div>
+                                    <div className="item-price">$2.50</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="container-bottom" />
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
