dfb25bbf7e8b0c5441fa5a1ec93578ace3b06bae
Jokūbas Šilgalys <jokubas.silgalys@gmail.com>
2024-11-29 15:41:42 +0200

    Updated payment page

--- client/src/payment.tsx
+++ client/src/payment.tsx
@@ -1,12 +1,74 @@
-import './payment.css';
+import { GoArrowLeft } from "react-icons/go";
+import { useNavigate } from 'react-router-dom';
 import TopNav from './top-nav.tsx';
+import './payment.css';
+
 function Payment() {
+    const navigate = useNavigate();
 
     return (
         <>
             <TopNav />
 
             <div id="payment-container">
+                <div id="payment-container-top">
+                    <div id="payment-title">
+                        <GoArrowLeft id="back-arrow" size={30} onClick={() => navigate("/")} />
+                        <div id="title-content">
+                            <div>Pay Full Amount</div>
+                            <div id="title-amount">($16.00)</div>
+                        </div>
+                    </div>
+                    <hr />
+                </div>
+                <div id="payment-container-middle">
+                    <div id="container-middle-left">
+                        <div id="middle-left-title">ADD TIP</div>
+                        <div id="middle-left-tips">
+                            <div className="tips">
+                                No tip
+                            </div>
+                            <div className="tips">
+                                15% Ok!
+                            </div>
+                            <div className="tips">
+                                18% Good!
+                            </div>
+                            <div className="tips">
+                                20% Great!
+                            </div>
+                            <div className="tips">
+                                25% Wow!
+                            </div>
+                            <div className="tips">
+                                Custom tip
+                            </div>
+                        </div>
+                        <div id="middle-left-payment">
+                            <div id="payment-price">
+                                <div id="prices">
+                                    <div className="price-component">
+                                        <div className="component-name">Payment</div>
+                                        <div className="price-amount">$16.00</div>
+                                    </div>
+                                    <div className="price-component">
+                                        <div className="component-name">Tip</div>
+                                        <div className="price-amount">$4.00</div>
+                                    </div>
+                                </div>
+                                <hr />
+                                <div id="price-bottom">
+                                    <div className="component-name">Total</div>
+                                    <div className="price-amount">$20.00</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="container-middle-right">
+                    </div>
+                </div>
+                <div id="payment-container-bottom">
+                </div>
             </div>
         </>
     )
