c4d443448337226b582686695f8581cf3e4fa2a2
emika7 <ekrajeckaite@gmail.com>
2024-12-10 00:21:31 +0200

    added login page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import './main.css';
+
+import LoginPage from './LoginPage';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} /> {/* Login Page as default */}
+      </Routes>
+    </Router>
+  </StrictMode>
+);
