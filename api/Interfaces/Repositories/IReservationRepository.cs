3f4b2a0662e267dae45e105baa6e852a8c4fad19
Juras Ezerskis <ezerskis.juras@gmail.com>
2024-12-17 19:33:36 +0200

    Finished Reservations and Service. I've updated the code to ensure that an employee can only be assigned to one reservation at a time, and each service can only have one active instance at any given time.

--- api/Interfaces/Repositories/IReservationRepository.cs
+++ api/Interfaces/Repositories/IReservationRepository.cs
@@ -10,5 +10,8 @@ namespace api.Interfaces.Repositories
         Task UpdateAsync(Reservation reservation);
         Task DeleteAsync(Reservation reservation);
         Task<IEnumerable<Reservation>> GetReservationsByCustomerIdAsync(int customerId);
+        Task<IEnumerable<Reservation>> GetOverlappingReservationsForEmployeeAsync(int employeeId, DateTime startTime, DateTime endTime, int? excludeReservationId = null);
+        Task<IEnumerable<Reservation>> GetOverlappingReservationsForServiceAsync(int serviceId, DateTime startTime, DateTime endTime, int? excludeReservationId = null);
+
     }
 }
